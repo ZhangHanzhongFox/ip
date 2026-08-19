f66939758aecf86c7c5151529446afc84bea3abb
ZhangHanzhongFox <e1513113@u.nus.edu>
2026-08-19 21:22:32 +0800

    Rename chatbot to Fox and update banner

---
+++ src/main/java/Fox.java
@@ -0,0 +1,11 @@
+public class Fox {
+    public static void main(String[] args) {
+        String banner = " _____        _      \n"
+                + "|  ___|      | |     \n"
+                + "| |_ _____  _| |     \n"
+                + "|  _/ _ \\ \\/ / |     \n"
+                + "| || (_) >  <| |     \n"
+                + "|_| \\___/_/\\_\\_|     \n";
+        System.out.println(banner);
+    }
+}
