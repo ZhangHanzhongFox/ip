dd4826fadb018cd44d634754efdbd4d740c2e243
ZhangHanzhongFox <e1513113@u.nus.edu>
2026-08-20 22:22:15 +0800

    can add or list tasks

--- src/main/java/Fox.java
+++ src/main/java/Fox.java
@@ -4,6 +4,10 @@ public class Fox {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        // Array for task storage
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         String greeting = "  /\\_/\\\n"
                 + " ( •ᴗ• )   Hi! I'm Fox, your little companion. 🦊\n"
                 + "  > ^ <    I may be small, but I've got plenty of tricks up my sleeve.\n"
@@ -22,12 +26,21 @@ public class Fox {
         while (true) {
             String command = scanner.nextLine();
 
-            System.out.println(command);
-            System.out.println("------------------------------------------------------------");
+            System.out.println("     ------------------------------------------------------------");
 
             if (command.equals("bye")) {
                 System.out.print(farewell);
                 break;
+            } else if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println("     " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println("     ------------------------------------------------------------");
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("     added: " + command);
+                System.out.println("     ------------------------------------------------------------");
             }
         }
 
